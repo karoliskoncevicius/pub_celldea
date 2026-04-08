90682c961d79fade047c30fdfd1d066ab93c0506
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2026-04-08 12:09:09 +0300

    updated EWAS with the addition of 2 manual studies

--- data/EWAScatalog/ewascatalog_blood.r
+++ data/EWAScatalog/ewascatalog_blood.r
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 
+library(annmatrix)
 suppressPackageStartupMessages(library(inops))
+suppressPackageStartupMessages(library(readxl))
 
 res <- read.delim("input/ewascatalog-results.txt")
 std <- read.delim("input/ewascatalog-studies.txt")
@@ -61,6 +63,20 @@ ewas <- ewas[!grepl("cell composition \\(reference free\\)", ewas$Covariates),]
 
 ewas <- ewas[,c("StudyID", "PMID", "Author", "Year", "Trait", "Age", "Covariates", "N", "CpG", "P", "Beta")]
 colnames(ewas) <- tolower(colnames(ewas))
+
+
+#----- add ewas manually -------------------------------------------------------
+
+# NOTE: adding additional EWAS that adjusted for 12 cell types (reportedly)
+
+e <- as.data.frame(read_excel("input/KEPI_A_2289786_SM2258.xls", skip = 5))
+e <- data.frame(studyid = "38090774_crohns", pmid = "38090774", author = "Reynolds", year = "2024", trait = "crohn's disease", age = "pediatric", covariates = NA, n = NA, cpg = e$CpGID, p = NA, beta = NA)
+ewas <- rbind(ewas, e)
+
+e <- as.data.frame(read_excel("input/42003_2026_9520_MOESM4_ESM.xlsx", sheet = 2, skip = 3))
+e <- data.frame(studyid = "41639309_interleukin", pmid = "41639309", author = "Sinke", year = "2026", trait = "interleukin-6", age = "adult", covariates = NA, n = NA, cpg = e$ProbeID, p = e[,"padj (cells)"], beta = e[,"Beta (cells)"])
+e <- e[e$p < 0.05,]
+ewas <- rbind(ewas, e)
 
 
 #--- save ----------------------------------------------------------------------
