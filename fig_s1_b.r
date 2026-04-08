ce29e7d4a78809ab976a56d3f6dca90875a21b75
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2026-04-08 18:36:59 +0300

    added fig s1

---
+++ fig_s1_b.r
@@ -0,0 +1,43 @@
+#--- init ----------------------------------------------------------------------
+
+library(kklibrary)
+
+dfage <- redo("data/diffage_cgs_rhead.rds")
+
+
+#--- age direction overlaps ----------------------------------------------------
+
+dfage <- dfage[dfage$celldea_qvalue <= 0.05,]
+
+sigs <- t(apply(dfage[,5:7], 1, function(x) p.adjust(x, "fdr"))) <= 0.05
+
+agedir <- ifelse(sigs[,"pvalue_mono"], "mono", "")
+agedir <- paste0(agedir, ifelse(sigs[,"pvalue_tcd4n"], " tcd4n", ""))
+agedir <- paste0(agedir, ifelse(sigs[,"pvalue_tcd4m"], " tcd4m", ""))
+agedir <- trimws(agedir)
+agedir[agedir == ""] <- "none"
+
+tab <- table(agedir)
+tab <- sort(tab, decreasing = TRUE)
+
+
+#--- plot ----------------------------------------------------------------------
+
+pdf("fig_s1_b.pdf", width = 14/2.54, height = 6/2.54, pointsize = 8)
+
+par(mar = c(1,5,1,1), tck=-0.01, mgp=c(2,0.5,0))
+
+plot.new()
+plot.window(xlim = c(0, 9), ylim = c(-11000,20000), xaxs = "i", yaxs = "i")
+
+rect((1:8) - 0.46, 0, (1:8) + 0.45, tab, lwd = 2)
+axis(2, at = seq(0,20000,5000), las = 1)
+title(ylab = "Number of cytosines", line = 3.5, adj = 0.85)
+
+cols <- c("white", cell2col("mono"))[grepl("mono", names(tab))+1]
+cols <- c(cols, c("white", cell2col("tcd4n"))[grepl("tcd4n", names(tab))+1])
+cols <- c(cols, c("white", cell2col("tcd4m"))[grepl("tcd4m", names(tab))+1])
+symbols(rep(1:8, 3), rep(-c(3000,6000,9000), each = 8), circles = rep(0.19, 3*8), bg = cols, add = TRUE, inches = FALSE)
+text(0, -c(3000,6000,9000), c("Mono", "TCD4n", "TCD4m"), pos = 2, xpd = NA)
+
+invisible(dev.off())
